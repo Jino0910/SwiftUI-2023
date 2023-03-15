6d4eb2763af258adfedbc8dfa4bae248a3528577
gray <gray.b@kakaocorp.com>
2023-03-15 13:11:00 +0900

    [Add] SwiftUIAsyncImage

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by kakao on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
