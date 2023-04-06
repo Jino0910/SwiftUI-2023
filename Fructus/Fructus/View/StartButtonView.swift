74c6ea385ee51f00fc6a1fadb6606011cea45e38
gray <gray.b@kakaocorp.com>
2023-04-06 19:40:31 +0900

    [Add] Onboarding Page TabView

---
+++ Fructus/Fructus/View/StartButtonView.swift
@@ -0,0 +1,40 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by kakao on 2023/04/06.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Button {
+            print("Exit the onvoarding")
+        } label: {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(
+                Capsule().strokeBorder(Color.white, lineWidth: 1.25)
+            )
+        }
+        .accentColor(Color.white)
+    }
+}
+
+// MARK: - PREVIEW
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
