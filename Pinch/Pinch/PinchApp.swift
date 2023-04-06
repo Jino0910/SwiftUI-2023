5223c2e9d99e9bee0800f4731908bf5e4f0ecabd
gray <gray.b@kakaocorp.com>
2023-04-06 13:08:11 +0900

    [Add] New Pinch Project

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by kakao on 2023/04/06.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
